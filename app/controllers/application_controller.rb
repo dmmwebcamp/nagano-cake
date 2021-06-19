ad5ea5e99cd27e1624d811308f730683ef150958
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-06-19 09:17:32 +0000

    finish_customer_header_footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :get_current_customer
+
+  def get_current_customer
+    @customer = current_customer
+  end
 end
