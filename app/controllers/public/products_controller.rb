cffa203b5bf8700176b8ad4b1c261d54565174bf
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-06-23 06:58:08 +0000

    cart_productほぼ終わり

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,8 +1,13 @@
 class Public::ProductsController < Public::Base
-    
+
     def index
+        @products = Product.all
     end
 
     def show
+        @product = Product.find(params[:id])
+        @cart_product = CartProduct.new
     end
+
+    
 end
