8d790a682e2426d54174ccfb5198ce5223a687e5
tomoya530 <ec2-user@ip-172-31-15-227.ec2.internal>
2021-06-24 08:43:57 +0000

    Finish.pro.lay

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,10 +1,10 @@
 class Public::CartProductsController < Public::Base
-  
-def index
+
+   def index
       @customer = current_customer
       @cart_products = CartProduct.all
       @numbers = (1..100).to_a
-      
+
    end
 
    def update
@@ -36,7 +36,7 @@ def index
          @cart_product.save
       end
       redirect_to cart_products_path
-      
+
    end
 
    private
