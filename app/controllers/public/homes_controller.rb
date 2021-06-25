4559170ddd96dad0b7725d09e8e6910e5d82877d
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-06-25 02:16:49 +0000

    top_feature finish

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,9 @@
 class Public::HomesController < Public::Base
 
     def top
-        @products = Product.limit(4).order("id")
+        @product = Product.order("RANDOM()").limit(1)
+        @products = Product.order("RANDOM()").limit(4)
+        # @products = Product.limit(4).order("id")
     end
 
     def about
